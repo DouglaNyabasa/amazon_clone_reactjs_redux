c8fd42914b1a98d3a6516a3f3e8dd5c564c4beca
douglanyabasa <douglasnyabasa400@gmail.com>
2025-08-10 13:20:48 +0200

    finished product details page

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import amazon from "../../assets/images/amazon.png";
 import { ShoppingCartIcon } from "@heroicons/react/24/outline";
 import { Search } from "../Home";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="min-w-[1000px]">
       <div className="flex bg-amazonClone text-white h-[60px]">
         <div className="flex items-center m-4">
-          <img className="h-[35px] w-[100px] m-2" src={amazon} alt="" />
+          <Link href="href" to={"/"}>
+            {" "}
+            <img className="h-[35px] w-[100px] m-2" src={amazon} alt="" />
+          </Link>
           <div className="pr-4 pl-4">
             <div className="text-xs xl:text-sm">Deliver to</div>
             <div className="text-sm xl:text-base font-bold">Zimbabwe</div>
@@ -31,10 +35,12 @@ const Navbar = () => {
             <div className="text-xs xl:text-sm">Returns</div>
             <div className="text-sm xl:text-base font-bold">& Orders</div>
           </div>
-          <div className="flex pr-3 pl-3">
+          <Link href="href" to={"/checkout"}>
+           <div className="flex pr-3 pl-3">
             <ShoppingCartIcon className="h-[48px]" />
             <div className="mt-7 text-xs xl:text-sm">Cart</div>
           </div>
+          </Link>
         </div>
       </div>
       <div className="flex bg-amazonClone-light_blue text-white text-xs space-x-3 xl:text-sm p-2 pl-6">
--- src/componets/ProductPage/ProductPage.jsx
+++ src/componets/ProductPage/ProductPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { callAPI } from "../Utils/CallApis";
 import { ProductDetails } from "../Home";
+import {GB_CURRENCY  } from '../Utils/Constants'
 
 const ProductPage = () => {
   const [product, setProduct] = useState(null);
@@ -21,7 +22,7 @@ const ProductPage = () => {
   return (
     product && (
       <div className="h-screen bg-amazonClone-background">
-        <div className="min-w-[1000px] max-w-[1500px] m-auto bg-orange-400">
+        <div className="min-w-[1000px] max-w-[1500px] m-auto p-4">
           <div className="grid grid-cols-10 gap-2">
             {/* left */}
             <div className="col-span-3 p-8 rounded bg-white m-auto">
@@ -36,20 +37,20 @@ const ProductPage = () => {
             </div>
             {/* right */}
             <div className="col-span-2 bg-white rounded p-4">
-              <div className="text-xl xl:text-2xl font-semibold">{product.price}</div>
-              <div className="">{product.oldPrice}</div>
-              <div className="">FREE Returts</div>
-              <div className="">FREE Delivery</div>
-              <div className="">In Stock</div>
-              <div className="">
+              <div className="text-xl xl:text-2xl font-semibold text-red-700 text-right">{GB_CURRENCY.format(product.price)}</div>
+              <div className="text-base xl:text-lg font-semibold text-gray-500 text-right">RRP: <span className="line-through">{GB_CURRENCY.format(product.oldPrice)}</span></div>
+              <div className="text-sm xl:text-base font-semibold text-blue-500 mt-3">FREE Returts</div>
+              <div className="text-sm xl:text-base font-semibold text-blue-500 mt-1">FREE Delivery</div>
+              <div className="text-base xl:text-lg font-semibold text-green-500 mt-1">In Stock</div>
+              <div className="text-base xl:text-lg font-semibold mt-1">
                 Quantity:
-                <select>
+                <select className="p-2 bg-white border rounded-md focus:border-indigo-600">
                   <option>1</option>
-                  <option>2</option>
+                  <option>2</option> 
                   <option>3</option>
                 </select>
               </div>
-              <button>Add to Cart</button>
+              <button className="bg-yellow-400 w-full p-3 text-xs xl:text-sm rounded hover:bg-yellow-500 mt-4">Add to Cart</button>
             </div>
           </div>
         </div>
