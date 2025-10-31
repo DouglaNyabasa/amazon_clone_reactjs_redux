14f19a7689efa3a6e9d3341cf2ef665f550bbf82
douglanyabasa <douglasnyabasa400@gmail.com>
2025-10-31 12:26:40 +0200

    updated fix for deployement

--- src/componets/Home/HomePage.jsx
+++ src/componets/Home/HomePage.jsx
@@ -1,70 +1,80 @@
+import React from "react";
 import Carousel from "../Carousel/Carousel";
+import HomePageCard from "../HomePageCard/HomePageCard";
+import cardImg1 from "../../../public/images/home_grid_1.jpg";
+import cardImg2 from "../../../public/images/home_grid_2.jpg";
+import cardImg3 from "../../../public/images/home_grid_3.jpg";
+import cardImg4 from "../../../public/images/home_grid_4.jpg";
+import cardImg5 from "../../../public/images/home_grid_5.jpg";
+import cardImg6 from "../../../public/images/home_grid_6.jpg";
+import cardImg7 from "../../../public/images/home_grid_7.jpg";
+import cardImg8 from "../../../public/images/home_grid_8.jpg";
+import bannerImg from "../../../public/images/banner2.jpg";
+import bannerImg1 from "../../../public/images/banner_image.jpg";
 import CarouselCategory from "../Carousel/CarouselCategory";
 import CarouselProducts from "../Carousel/CarouselProducts";
-import HomePageCard from "../HomePageCard/HomePageCard";
+
+
+
+
+
 
 const HomePage = () => {
   return (
-    <div className="bg-amazonclone-background">
-      <div className="min-w-[1000px] max-w-[1500px] m-auto">
+    <div className="bg-amazonClone-background">
+      <div className="min-w-[1000px] max-w-[1500px]  m-auto">
         <Carousel />
         <div className="grid grid-cols-3 xl:grid-cols-4 -mt-80">
           <HomePageCard
             title={"We have a surprise for you"}
-            img={"../../../public/images/home_grid_1.jpg"}
+            img={cardImg1}
             link={"See terms and conditions"}
           />
-          <HomePageCard
-            title={"Watch The Rings of Power"}
-            img={"../../../public/images/home_grid_2.jpg"}
+           <HomePageCard
+            title={"Watch the rings of power"}
+            img={cardImg2}
             link={"Start streaming now"}
           />
-          <HomePageCard
+           <HomePageCard
             title={"Unlimited Streaming"}
-            img={"../../../public/images/home_grid_3.jpg"}
+            img={cardImg3}
             link={"Find out more"}
           />
-          <HomePageCard
+           <HomePageCard
             title={"More titles to explore"}
-            img={"../../../public/images/home_grid_4.jpg"}
+            img={cardImg4}
             link={"Browse Kindle Unlimited"}
           />
-          <HomePageCard
+           <HomePageCard
             title={"Shop Pet Supplies"}
-            img={"../../../public/images/home_grid_5.jpg"}
+            img={cardImg5}
             link={"See more"}
           />
-          <HomePageCard
-            title={"Spring Sale"}
-            img={"../../../public/images/home_grid_6.jpg"}
+           <HomePageCard
+            title={"Spring sale"}
+            img={cardImg6}
             link={"See the deals"}
           />
-          <HomePageCard
-            title={"Echo Buds"}
-            img={"../../../public/images/home_grid_7.jpg"}
+           <HomePageCard
+            title={"Echo buds"}
+            img={cardImg7}
             link={"See more"}
           />
-          <HomePageCard
-            title={"Family Plan: 3 months free"}
-            img={"../../../public/images/home_grid_8.jpg"}
+           <HomePageCard
+            title={"Family plan: 3 months free"}
+            img={cardImg8}
             link={"Learn more"}
           />
-          <div className="m-3 pt-8">
-            <img
-              className="xl:hidden"
-              src={"../../../public/images/banner_image_2.jpg"}
-              alt="Banner 2"
-            />
+
+          <div className="p-2 pt-8">
+            <img className="xl:hidden" src={bannerImg} alt=""/>
           </div>
+
         </div>
-        <CarouselProducts />
-        <CarouselCategory />
-        <div className="h-[200px]">
-          <img
-            className="h-[100%] m-auto"
-            src={"../../../public/images/banner_image.jpg"}
-            alt="Banner 1"
-          />
+        <CarouselProducts/>
+        <CarouselCategory/>
+        <div className=" h-[200px] ">
+           <img className="h-[100%] m-auto" src={bannerImg1} alt=""/>
         </div>
       </div>
     </div>
